d536362aee4be8a87ef07ac28275381d59012575
uniquemozilla22 <uniqgaming26@gmail.com>
2022-05-19 21:54:26 +0545

    Sending and Receiving the message Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
 import "./App.css";
 import io from "socket.io-client";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const socket = io.connect("http://localhost:8000");
 
 function App() {
-  const [message, setMessage] = useState(null);
+  const [messageInput, setMessageInput] = useState([]);
+
+  const [message, setMessage] = useState([]);
+
   const sendMessage = (e) => {
     e.preventDefault();
     socket.emit("send_message", { message });
+    setMessage([...message, messageInput]);
   };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessage([...message, data.message]);
+      console.log(data);
+    });
+  }, [socket]);
 
   return (
     <div className="App">
+      {message.map((m) => (
+        <>
+          {m}
+          <br />
+        </>
+      ))}
       <form onSubmit={(e) => sendMessage(e)}>
         <input
           type="text"
           placeholder="Message"
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={(e) => setMessageInput(e.target.value)}
         />
         <input type="submit" value={"Send"} />
       </form>
